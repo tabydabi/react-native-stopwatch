dfb62ab65a6761dc31c8fe36f45bf039b6dbf001
Kirill Borun <69752848+tabydabi@users.noreply.github.com>
2021-10-07 10:03:13 +0300

    feat add validation form

---
+++ src/components/Button/Button.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import {Button} from 'react-native';
+
+export const ButtonB = () => {
+  return <Button></Button>;
+};
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,4 +1,3 @@
-// Formik x React Native example
 import React from 'react';
 import {
   SafeAreaView,
@@ -8,21 +7,78 @@ import {
   Text,
   StatusBar,
   TextInput,
+  Button,
 } from 'react-native';
 import {Formik} from 'formik';
+import * as Yup from 'yup';
 
 export const MyReactNativeForm = () => {
   return (
     <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView style={styles.container}>
-        <View style={styles.loginContainer}>
-          <Text>Login Screen</Text>
-        </View>
-      </SafeAreaView>
+      <Text>Sign Up</Text>
+      <Formik
+        initialValues={{name: '', email: '', password: ''}}
+        validationSchema={validationSchema}
+        onSubmit={values => console.log(values)}>
+        {({
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          values,
+          errors,
+          touched,
+        }) => (
+          <View>
+            <TextInput
+              placeholder="Name"
+              onChangeText={handleChange('name')}
+              onBlur={handleBlur('name')}
+              value={values.name}
+            />
+            {errors.name && touched.name && (
+              <Text style={{color: 'red'}}>{errors.name}</Text>
+            )}
+            <TextInput
+              placeholder="Email"
+              onChangeText={handleChange('email')}
+              onBlur={handleBlur('email')}
+              value={values.email}
+            />
+            {errors.email && touched.email && (
+              <Text style={{color: 'red'}}>{errors.email}</Text>
+            )}
+            <TextInput
+              placeholder="Password"
+              onChangeText={handleChange('password')}
+              onBlur={handleBlur('password')}
+              value={values.password}
+            />
+            {errors.password && touched.password && (
+              <Text style={{color: 'red'}}>{errors.password}</Text>
+            )}
+
+            <Button onPress={handleSubmit} title="Submit" />
+          </View>
+        )}
+      </Formik>
     </>
   );
 };
+
+const validationSchema = Yup.object().shape({
+  name: Yup.string().required('Name is required').label('Name'),
+  email: Yup.string()
+    .email('Please enter valid email')
+    .required('Email is required')
+    .label('Email'),
+  password: Yup.string()
+    .matches(/\w*[a-z]\w*/, 'Password must have a small letter')
+    .matches(/\w*[A-Z]\w*/, 'Password must have a capital letter')
+    .matches(/\d/, 'Password must have a number')
+    .min(8, ({min}) => `Password must be at least ${min} characters`)
+    .required('Password is required')
+    .label('Password'),
+});
 
 const styles = StyleSheet.create({
   container: {
